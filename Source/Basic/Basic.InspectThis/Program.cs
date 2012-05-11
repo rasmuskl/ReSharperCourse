afc388e45b05f1c892b93a45a7ac41d3935049cc
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2012-05-12 00:48:50 +0200

    Converted tasks to .html and added Inspect This and LongestClassName projects.

---
+++ Source/Basic/Basic.InspectThis/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace Basic.InspectThis
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int importantValue = 42;
+
+            foreach (var value in GetAllValues(importantValue))
+            {
+                Console.WriteLine("Value: {0}", value);
+            }
+
+            importantValue *= 2;
+            Console.Out.WriteLine(importantValue);
+        }
+
+        private static IEnumerable<int> GetAllValues(int importantValue)
+        {
+            var valueList = new List<int>();
+
+            valueList.Add(importantValue);
+            valueList.AddRange(GetOtherValues());
+
+            return valueList;
+        }
+       
+        private static IEnumerable<int> GetOtherValues()
+        {
+            return new[] { 1, 3, 5, 7 };
+        }
+    }
+}
