e2fff5493047839feb7f8f1019ee6f11afd81056
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2012-05-12 13:39:48 +0200

    Added reorder task and extracted keys into a helper.

---
+++ Source/Basic/Basic.ScratchPad/OutOfOrderMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic.ScratchPad
+{
+    public class OutOfOrderMethods
+    {
+        public void D()
+        {
+        }
+
+        public void E()
+        {
+        }
+
+        public void C()
+        {
+        }
+
+        public void B()
+        {
+        }
+
+        public void A()
+        {
+        }
+    }
+}
