c51cf3240d3085e2a64e30f650de2c48a929b106
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2012-05-16 12:15:37 +0200

    Added some simple comboes to the writing code in Basic.

---
+++ Source/Basic/Basic/WritingCode/SimpleCombos.cs
@@ -0,0 +1,23 @@
+﻿namespace Basic.WritingCode
+{
+    public class SimpleCombos
+    {
+        public void IntroduceVariable(string userInput)
+        {
+            int x = 42 + 1024 * int.Parse(userInput);
+        }
+
+        public void ImportCompletion()
+        {
+            // Extension methods
+            // SecurityClassification
+        }
+
+        public void IntroduceVariableImportCompletionCombo()
+        {
+            // new HybridDictionary()
+            // new BlockingCollection<ConcurrentQueue<Guid>>()
+            // CompressionMode.Compress
+        }
+    }
+}
