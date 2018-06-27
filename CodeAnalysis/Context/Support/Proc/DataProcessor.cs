f9119b8912177749cf3377528961410050d5dfac
Alito Alvarez <alito.alvarez@asos.com>
2018-06-27 18:20:30 +0100

    Completed exercise 3, added context (codeanalysis) solution/project.

---
+++ CodeAnalysis/Context/Support/Proc/DataProcessor.cs
@@ -0,0 +1,12 @@
+namespace Context.Support.Proc
+{
+    using System;
+
+    public class DataProcessor
+    {
+        public static void DoProcessing(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
